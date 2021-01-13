2d0ccd539b565df980bf3c0b75fea59ef7958280
Mustafa <mustafa.abdelbadea@gmail.com>
2021-01-13 17:01:15 +0200

    add component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { RequestResetComponent } from './request-reset/request-reset.component';
+import { ResponseResetComponent } from './response-reset/response-reset.component';
+import {SigninComponent} from './signin/signin.component'
+import { VerifyEmailComponent } from './verify-email/verify-email.component';
+const routes: Routes = [
+  {path:'signin',component:SigninComponent},
+    {path:'requestreset',component:RequestResetComponent},
+    {path:'pharmacyForgotPassword/:token',component:ResponseResetComponent},
+    {path:'pharmacyVerifyEmail/:token',component:VerifyEmailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {ReactiveFormsModule, FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SigninComponent } from './signin/signin.component';
+import { RequestResetComponent } from './request-reset/request-reset.component';
+import { ResponseResetComponent } from './response-reset/response-reset.component';
+import { VerifyEmailComponent } from './verify-email/verify-email.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent,
+    RequestResetComponent,
+    ResponseResetComponent,
+    VerifyEmailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

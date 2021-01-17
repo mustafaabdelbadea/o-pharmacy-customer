72964e6482ba6bd92dd9ba6258fc655fb905cc70
Mustafa <mustafa.abdelbadea@gmail.com>
2021-01-17 03:00:59 +0200

    send token to header

--- src/app/services/report.service.ts
+++ src/app/services/report.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders  } from '@angular/common/http';
 import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class ReportService {
+
   baseUrl = "http://localhost:3000/";
   constructor(private _http: HttpClient) {
   }
+
   reportProblem(problemData: any): Observable<any> {
-    return this._http.post(this.baseUrl + "reportProblem", problemData);
+    return this._http.post(this.baseUrl + "reportProblem", problemData,{ headers: new HttpHeaders({'token': 'Bearer' + localStorage.getItem('token')})
+  });
   }
 
 }

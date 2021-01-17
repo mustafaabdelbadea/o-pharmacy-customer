0e49aee5042fc9e5fd035617068b0cc4d654e44c
abdallhmz99 <abdallhmz99@gmail.com>
2021-01-17 05:51:16 +0200

    try headers

--- src/app/services/get-health-news.service.ts
+++ src/app/services/get-health-news.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 
-import{HttpClient}from'@angular/common/http'
+import{HttpClient, HttpHeaders}from'@angular/common/http'
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -8,11 +8,19 @@ import { Observable } from 'rxjs';
 })
 export class GetHealthNewsService {
 
+  // httpOptions = {
+  //   headers: new HttpHeaders({
+  //     'token':'5fb6890f4331ab10881bf2fc'
+  //   }) 
+      
+  // }
+
   constructor(public _HttpClient:HttpClient) { }
 
   getnews():Observable<any>
 {
   return this._HttpClient.get("http://newsapi.org/v2/top-headlines?country=eg&category=health&apiKey=995310e0ca154ddb8c28bb4be0c6df68")
+  //return this._HttpClient.get("http://localhost:3000/ourPharmacies",this.httpOptions)
 }
 
 }

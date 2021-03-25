58cd373b584be33f1e335be7e830346dc59df623
Mustafa <mustafa.abdelbadea@gmail.com>
2021-03-25 17:38:45 +0200

    request reset

--- src/app/request-reset/request-reset.component.ts
+++ src/app/request-reset/request-reset.component.ts
@@ -9,23 +9,35 @@ import { AuthService } from "../services/auth.service";
 })
 export class RequestResetComponent implements OnInit {
   //take email from form
+  isClicked:any=false;
+  isSuccess:any;
+  responseMessage:any;
   resetForm = new FormGroup({
     'email': new FormControl(null, [Validators.required, Validators.email]),
   });
   //call the service
   ResetPass() {
+    this.isClicked=true;
     this._AuthService.resetPassword(this.resetForm.value).subscribe(data => {
       console.log(data);
-      if (data.message == 'success') {
-        localStorage.setItem('token', data.token);
-        this._Router.navigateByUrl("/home");
+      if (data.message == 'Email Sent') {
+        this.isClicked=false;
+        this.isSuccess=true;
+        this.responseMessage=data.message;
       }
       else if (data.message == 'email not Verified') {
+        this.isClicked=false;
+        this.isSuccess=true;
+        this.responseMessage='Verify Your Account';
         console.log('verify email')
       }
-      else {
+      else if (data.message='Email Not Found') {
+        this.isClicked=false;
+        this.isSuccess=true;
+        this.responseMessage='Email Not Found';
         console.log(data.message)
-      }
+
+      } 
     },
       err => {
         console.log(err);

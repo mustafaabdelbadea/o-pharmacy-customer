c9f2963cdbf752ee723fa30b43c1c2fa75b46367
abdallhmz99 <abdallhmz99@gmail.com>
2021-03-28 02:34:18 +0200

    edit getOnePharmacy and finish gitOneOrder and edit currentOrders

--- src/app/customer-current-orders/customer-current-orders.component.ts
+++ src/app/customer-current-orders/customer-current-orders.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import{CustomerCurrentOrdersService} from '../services/customer-current-orders.service';
+import * as mapboxgl from 'mapbox-gl';
+
 @Component({
   selector: 'app-customer-current-orders',
   templateUrl: './customer-current-orders.component.html',
@@ -9,28 +11,66 @@ export class CustomerCurrentOrdersComponent implements OnInit {
 
 
   currentOrderData: any;
+  pharmacyData:any;
+  orderDate:any;
+  orderTime:any;
+  lng: any;
+  latt: any;
+
+
+
 
   constructor(private _CustomerCurrentOrdersService:CustomerCurrentOrdersService) {
+    setTimeout(() => {
 
-_CustomerCurrentOrdersService.currentOrder().subscribe( (data)=>{
+  console.log("ay haga");
+  _CustomerCurrentOrdersService.currentOrder().subscribe( (data)=>{
         
 
-  if (data.message=='success') {
-    this.currentOrderData=data.customerOrders;
-  }
-  else if (data.message="no order founds") {
-    this.currentOrderData=null;   
-    console.log(this.currentOrderData);
-  }
-
-},
-err => {
-  console.log(err);
-} )
+    if (data.message=='success') {
+      this.currentOrderData=data.customerOrders[0];
+      this.pharmacyData=data.pharmacyData;
+      console.log(data);
+    }
+    else if (data.message="no order founds") {
+      this.currentOrderData=null;   
+      this.pharmacyData=null;
+      console.log(this.currentOrderData);
+      console.log(data);
+    }
+  
+    this.lng=this.pharmacyData.locationAsCoordinates.coordinates.lon;
+    this.latt=this.pharmacyData.locationAsCoordinates.coordinates.lat;
+    this.orderDate=this.currentOrderData.date.substring(0,10);
+    this.orderTime=this.currentOrderData.date.substring(11,19);
+  //  this.map();
+  },
+  err => {
+    console.log(err);
+  } )
+  }, 5000);
 
    }
 
+   map() {
+    console.log(this.lng,this.latt)
+    const accessToken = 'pk.eyJ1IjoibXVzdGFmYWFiZGVsYmFkZWEiLCJhIjoiY2tpbHcwNmg2MG0wNjJ2cDlxbXI2NGZxbSJ9.h5Kephiwr11YMCfLXs14FQ';
+    var map = new mapboxgl.Map({
+      accessToken,
+      container: 'map', // container id
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [this.lng, this.latt], // starting position
+      zoom: 10,// starting zoom
+      trackResize: true
+    });
+    var marker1 = new mapboxgl.Marker()
+        .setLngLat([this.lng,this.latt])
+        .addTo(map);
+
+  }   
+
   ngOnInit(): void {
+   
   }
 
 }
--- src/app/get-one-order/get-one-order.component.ts
+++ src/app/get-one-order/get-one-order.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { OrderHistoryService } from '../services/order-history.service';
 import{ActivatedRoute}from '@angular/router';
+import * as mapboxgl from 'mapbox-gl';
+
+
 @Component({
   selector: 'app-get-one-order',
   templateUrl: './get-one-order.component.html',
@@ -9,7 +12,15 @@ import{ActivatedRoute}from '@angular/router';
 export class GetOneOrderComponent implements OnInit {
 
   oneOrderData:any;
+  pharmacyData:any;
   orderId:any;
+  orderDate:any;
+  orderTime:any;
+  lng: any;
+  latt: any;
+  globalStatus:any;
+  orderByTexting:any;
+  orderByPhoto:any;
   constructor(_ActivatedRoute:ActivatedRoute,_orderHistoryService:OrderHistoryService) { 
     this.orderId =  _ActivatedRoute.snapshot.paramMap.get('currentOrder')
 
@@ -17,13 +28,48 @@ export class GetOneOrderComponent implements OnInit {
 
       (data)=>{
         
-        this.oneOrderData=data;    
-        console.log(data)
-},
+    
+        if (data.message=='success') {
+          this.oneOrderData=data.orderData;
+          this.pharmacyData=data.pharmacyData   ;
+                 console.log(data);
+        }
+        else if (data.message="no order founds") {
+          this.oneOrderData=null;
+          this.pharmacyData=null;
+          console.log(this.oneOrderData);
+          console.log(data);
+        }
+      
+        this.lng=this.pharmacyData.locationAsCoordinates.coordinates.lon;
+        this.latt=this.pharmacyData.locationAsCoordinates.coordinates.lat;
+        this.orderDate=this.oneOrderData.date.substring(0,10);
+        this.orderTime=this.oneOrderData.date.substring(11,19);
+        this.globalStatus=this.oneOrderData.globalStatus;
+        this.orderByTexting=this.oneOrderData.orderByTexting;
+        this.orderByPhoto=this.oneOrderData.orderByPhoto;
+        //this.map();
+      },
 (error)=>{
         console.log(error) ;
     })}
-    
+
+    map() {
+      console.log(this.lng,this.latt)
+      const accessToken = 'pk.eyJ1IjoibXVzdGFmYWFiZGVsYmFkZWEiLCJhIjoiY2tpbHcwNmg2MG0wNjJ2cDlxbXI2NGZxbSJ9.h5Kephiwr11YMCfLXs14FQ';
+      var map = new mapboxgl.Map({
+        accessToken,
+        container: 'map', // container id
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [this.lng, this.latt], // starting position
+        zoom: 10,// starting zoom
+        trackResize: true
+      });
+      var marker1 = new mapboxgl.Marker()
+          .setLngLat([this.lng,this.latt])
+          .addTo(map);
+  
+    }    
 
 
 ngOnInit(): void {

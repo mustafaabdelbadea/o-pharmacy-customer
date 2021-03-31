9cf47d9c7e04eed501c41fa38db602176dceaa62
Mustafa <mustafa.abdelbadea@gmail.com>
2021-03-31 16:00:47 +0200

    add report api

--- src/app/services/report.service.ts
+++ src/app/services/report.service.ts
@@ -9,6 +9,10 @@ export class ReportService {
   baseUrl = "http://localhost:3000/";
   constructor(private _http: HttpClient) {
   }
+  rate(rating:any): Observable<any> {
+    return this._http.post(this.baseUrl + "rate", rating,{ headers: new HttpHeaders({'token': 'Bearer' + localStorage.getItem('token')})
+  });
+  }
 
   reportProblem(problemData: any): Observable<any> {
     return this._http.post(this.baseUrl + "reportProblem", problemData,{ headers: new HttpHeaders({'token': 'Bearer' + localStorage.getItem('token')})
